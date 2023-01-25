a26307aee0822330a8f11d219332e7ac15c24086
mowoe <git@mowoe.com>
2023-01-25 20:36:30 +0100

    switch to theme colors

--- components/taskStatusBar.tsx
+++ components/taskStatusBar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { LayoutAnimation, StyleSheet, View } from 'react-native';
 import { Card, IconButton } from 'react-native-paper';
-import { OK_GREEN, WARNING_RED } from '../utils';
+import { useTheme } from 'react-native-paper';
 
 type StatusInfoProps = {
   variable: boolean;
@@ -13,6 +13,7 @@ type StatusInfoProps = {
 
 export const TaskStatusBar = ({variable, text, element, extraStatus, extraStatusVariable}: StatusInfoProps) => {
   const [expanded, setExpanded] = useState(false);
+  const theme = useTheme();
   const onButtonToggle = () => {
     if (!expanded) {
       LayoutAnimation.configureNext({
@@ -25,6 +26,58 @@ export const TaskStatusBar = ({variable, text, element, extraStatus, extraStatus
 
     setExpanded(!expanded);
   };
+
+  const styles = StyleSheet.create({
+    papertext: {
+      color: '#ffffff',
+    },
+    done: {
+      flex: 10,
+      paddingLeft: 10,
+      paddingRight: 10,
+      marginTop: 10,
+      backgroundColor: theme.colors.primary,
+    },
+    extra: {
+      flex: 10,
+      paddingLeft: 10,
+      paddingRight: 10,
+      marginTop: 10,
+      backgroundColor: '#ff8800',
+    },
+    todo: {
+      flex: 10,
+      paddingLeft: 10,
+      paddingRight: 10,
+      marginTop: 10,
+      backgroundColor: theme.colors.error,
+    },
+    visnone: {
+      display: 'none',
+    },
+    vis: {
+      display: 'flex',
+      width: '94%',
+      borderBottomEndL: 10,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+      backgroundColor: '#ededed',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      borderTopEndRadius: 0,
+      padding: 20,
+      marginBottom: 10,
+    },
+    expandbutton: {
+      borderWidth: 1,
+      flex: 2,
+    },
+    childcontainer: {
+      borderWidth: 1,
+    },
+  });
+  
+
   return (
     <>
       <Card
@@ -48,52 +101,3 @@ export const TaskStatusBar = ({variable, text, element, extraStatus, extraStatus
   );
 };
 
-const styles = StyleSheet.create({
-  papertext: {
-    color: '#ffffff',
-  },
-  done: {
-    flex: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 10,
-    backgroundColor: OK_GREEN,
-  },
-  extra: {
-    flex: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 10,
-    backgroundColor: '#ff8800',
-  },
-  todo: {
-    flex: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginTop: 10,
-    backgroundColor: WARNING_RED,
-  },
-  visnone: {
-    display: 'none',
-  },
-  vis: {
-    display: 'flex',
-    width: '94%',
-    borderBottomEndL: 10,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    backgroundColor: '#ededed',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    borderTopEndRadius: 0,
-    padding: 20,
-    marginBottom: 10,
-  },
-  expandbutton: {
-    borderWidth: 1,
-    flex: 2,
-  },
-  childcontainer: {
-    borderWidth: 1,
-  },
-});
