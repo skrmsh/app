1f62e4d7cd4e1db81371e4408b3d036ad4323735
mowoe <git@mowoe.com>
2023-01-25 20:31:21 +0100

    add errordialog component

---
+++ components/errorDialog.tsx
@@ -0,0 +1,33 @@
+import {Button, Dialog, Portal, Text, useTheme} from 'react-native-paper';
+
+type errorDialogProps = {
+  showingError: boolean;
+  setShowingError: (e: boolean) => void;
+  errorMsg: string;
+};
+export const ErrorDialog = ({
+  showingError,
+  setShowingError,
+  errorMsg,
+}: errorDialogProps) => {
+  const theme = useTheme();
+  return (
+    <Portal>
+      <Dialog visible={showingError} onDismiss={() => setShowingError(false)}>
+        <Dialog.Icon
+          icon="alert-octagon"
+          size={32}
+          color={theme.colors.error}
+        />
+        <Dialog.Content>
+          <Text variant="bodyMedium">{errorMsg}</Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button key={'closeButton'} onPress={() => setShowingError(false)}>
+            Close
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
+}
