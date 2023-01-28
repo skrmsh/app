2f67d7b0d2a60a6a0a41f200c02692c01bd2dd79
mowoe <git@mowoe.com>
2023-01-28 18:21:58 +0100

    remove font stuff

--- index.js
+++ index.js
@@ -15,27 +15,10 @@ import App from './App';
 import { name as appName } from './app.json';
 import React from 'react';
 
-const fontConfig = {
-  customVariant: {
-    fontFamily: Platform.select({
-      web: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
-      ios: 'System',
-      default: 'sans-serif',
-    }),
-    fontWeight: '400',
-    letterSpacing: 0.5,
-    lineHeight: 22,
-    fontSize: 20,
-  },
-};
-const theme = {
-  ...MD3LightTheme,
-  fonts: configureFonts({ config: fontConfig }),
-};
 export default function Main() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <PaperProvider theme={theme}>
+      <PaperProvider theme={MD3LightTheme}>
         <App />
       </PaperProvider>
     </SafeAreaView>
