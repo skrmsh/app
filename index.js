84f56dfaea84ab8f4f565a36b048ee04cea075f5
mowoe <git@mowoe.com>
2023-01-28 17:37:10 +0100

    eslint fixes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AppRegistry, SafeAreaView } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import App from './App';
 import { name as appName } from './app.json';
+import React from 'react';
 
 export default function Main() {
   return (
