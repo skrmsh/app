d845e42da57cf10856b956a017b23ded7aab4d43
mowoe <git@mowoe.com>
2023-04-10 16:18:57 +0200

    add new ble connection screen

--- components/bleConnectionScreen.tsx
+++ components/bleConnectionScreen.tsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import { BleConnection } from '.';
+import { BleManager, Device } from 'react-native-ble-plx';
+import { Button } from 'react-native-paper';
+import { BleConnection, ErrorDialog } from '.';
+import { getStyles } from '../utils';
 
-type BleConnectionScreenProps = {};
-export const BleConnectionScreen =
-  ({}: BleConnectionScreenProps): JSX.Element => {
-    return (
-      <>
-        <View style={{ margin: 15, height: '100%' }}>
-          <BleConnection
-            connectionIsFor="Phasor"
-            onConnectCallback={() => {}}
-            onDisconnectCallback={() => {}}
-          />
-        </View>
-      </>
-    );
-  };
+type BleConnectionScreenProps = {
+  manager: BleManager | undefined;
+  messageCallback: (message: string) => void;
+  connectedDevices: Device[];
+  setConnectedDevices: (setter: (devices: Device[]) => Device[]) => void;
+  onScreenFinishedCallback: () => void;
+};
+export const BleConnectionScreen = ({
+  manager,
+  messageCallback,
+  connectedDevices,
+  setConnectedDevices,
+  onScreenFinishedCallback,
+}: BleConnectionScreenProps): JSX.Element => {
+  const [errorMessage, setErrorMessage] = useState('');
+  return (
+    <>
+      <View
+        style={{
+          margin: 15,
+          height: '100%',
+          ...getStyles().centerContainer,
+        }}>
+        <ErrorDialog
+          showingError={!!setErrorMessage}
+          setShowingError={() => setErrorMessage('')}
+          errorMsg={errorMessage}
+        />
+        <BleConnection
+          connectionIsFor="Phasor"
+          onConnectCallback={() => {}}
+          onDisconnectCallback={() => {}}
+          manager={manager}
+          messageCallback={messageCallback}
+          errorMessageSetter={setErrorMessage}
+          connectedDevices={connectedDevices}
+          setConnectedDevices={setConnectedDevices}
+        />
+        <BleConnection
+          connectionIsFor="Vest"
+          onConnectCallback={() => {}}
+          onDisconnectCallback={() => {}}
+          manager={manager}
+          messageCallback={messageCallback}
+          errorMessageSetter={setErrorMessage}
+          connectedDevices={connectedDevices}
+          setConnectedDevices={setConnectedDevices}
+        />
+        <Button
+          onPress={onScreenFinishedCallback}
+          disabled={connectedDevices.length === 0}>
+          Continue
+        </Button>
+      </View>
+    </>
+  );
+};
