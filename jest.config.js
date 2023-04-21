22c80ee1a079d083b74f004f2007846a229cca0a
olel <olel@olel.de>
2023-04-21 19:25:44 +0200

    Fixed tests by adding asyncStorage mock

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     'node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)',
     'jest-runner',
   ],
+  setupFiles: [
+    './__mocks__/@react-native-async-storage/jestSetupAsyncStorage.js',
+  ],
 };
