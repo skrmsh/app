aba512b7c8f3c39129eb5413a94165f1676bec14
mowoe <git@mowoe.com>
2023-09-18 20:24:05 +0200

    move last function to api

--- utils/startGame.ts
+++ utils/startGame.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
-import { getHTTPUrl } from './helperFunctions';
+import { AxiosResponse } from 'axios';
+import { GameApi, GameGidPutRequest } from '../Api/generated';
+import { getApiConfiguration } from './helperFunctions';
 
 export function startGame(
   gameID: string,
@@ -10,21 +11,23 @@ export function startGame(
   callback: (e: AxiosResponse | void) => void,
   showError: (e: string) => void,
 ) {
+  const gameApi = new GameApi(
+    getApiConfiguration(serverHost, secureConnection),
+  );
   console.log(
     'making game start request to...',
-    `${getHTTPUrl(serverHost, secureConnection)}/game/${gameID}`,
+    `${serverHost}, ${secureConnection}/game/${gameID}`,
   );
   const config = {
     headers: {
       'x-access-token': authenticationToken,
     },
   };
-  axios
-    .put(
-      `${getHTTPUrl(serverHost, secureConnection)}/game/${gameID}`,
-      { delay: +gameStartDelay },
-      config,
-    )
+  const body: GameGidPutRequest = {
+    delay: +gameStartDelay,
+  };
+  gameApi
+    .gameGidPut(gameID, body)
     .then(response => {
       console.log(response);
       callback(response);
